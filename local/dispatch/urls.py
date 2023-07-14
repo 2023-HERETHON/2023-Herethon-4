2f28bcb38bb08b90607aa9b3a6bdd2ade523826d
hansun-hub <hansun33@naver.com>
2023-07-14 14:39:26 +0900

    feat: Add dispatch

---
+++ local/dispatch/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from .views import *
+app_name = "dispatch"
+
+urlpatterns = [
+    path('', views.whole_list, name='whole_list'),
+    path('near_list', views.near_list, name='near_list'),
+    path('my_list', views.my_list, name='my_list'),
+    path('post/', views.dispatch_post, name='dispatch_post'),
+    path('<int:pk>/', views.dispatch_detail, name="dispatch_detail"),
+
+    # path('<int:pk>/comment/', views.comments_create, name='comments_create'),
+
+]
