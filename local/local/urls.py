e37d7d1bcbd9c54c209580668ac2d5fce20c17c8
gardening-y <60008786@naver.com>
2023-07-14 14:14:08 +0900

    fix: html에 url 연결

--- local/local/urls.py
+++ local/local/urls.py
@@ -1,23 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-
-from lives import views
-
-
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
     path('lives/', include('lives.urls')),
-
     path('accounts/', include('allauth.urls')),
     path('users/', include('users.urls', namespace='users')),
-
     path('travels/', include('travels.urls')),
-
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
