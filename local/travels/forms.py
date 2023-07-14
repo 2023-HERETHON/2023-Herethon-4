d184378cd30a55e569b19cfd73ffcd2f51f4b79a
gardening-y <60008786@naver.com>
2023-07-15 03:25:21 +0900

    feat: travel detail 연결

--- local/travels/forms.py
+++ local/travels/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from .models import TravelComment
 
 class TravelCommentForm(forms.ModelForm):
-  content = forms.CharField(widget=forms.TextInput)
+  content = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': '댓글을 입력해주세요'})
+    )
   content.label = '' 
   class Meta:
     model = TravelComment
