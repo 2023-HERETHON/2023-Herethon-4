d3dfbf5108ab2a8d334d649583aa27d8146ea4f4
gardening-y <60008786@naver.com>
2023-07-15 02:02:58 +0900

    style: travel css 변경

--- local/lives/forms.py
+++ local/lives/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import VideoComment
 
 class VideoCommentForm(forms.ModelForm):
+  content = forms.CharField(widget=forms.TextInput)
+  content.label = '' 
   class Meta:
     model = VideoComment
     fields = ('content',)
+    labels = {'content': ''}
